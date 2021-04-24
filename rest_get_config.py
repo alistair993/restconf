ca05323e4486b32d4a68fe84d90a6626a8a39319
Alistair Key <alistair@Alistairs-MacBook-Pro-2.local>
2021-04-24 19:50:51 +0100

    Changed and uploaded

---
+++ rest_get_config.py
@@ -0,0 +1,15 @@
+import requests
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+url = 'https://192.168.0.100/restconf/data/native'
+headers = {'content-type': 'application/yang-data+json'}
+
+inUsername = input("Username: ")
+inUserPass = input("Password: ")
+
+response = requests.get(url, headers=headers, auth=(inUsername, inUserPass), verify=False)
+
+print(response)
+print(response.text)
